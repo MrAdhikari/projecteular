492b51e82948993ef26d79356f5aa5192b509168
Manish Adhikari <adhikari.manish555@gmail.com>
2025-04-04 23:26:10 +0545

    Update problem10.py

--- problem10.py
+++ problem10.py
@@ -1,11 +1,8 @@
 # Define the size limit
 limit = 2000000
 
-# Create a boolean list where index represents if the number is prime.
-# Initially, set all entries to True.
 is_prime = [True] * limit
 
-# 0 and 1 are not primes, so set them to False.
 is_prime[0] = is_prime[1] = False
 
 # Use the Sieve of Eratosthenes algorithm to mark non-prime numbers.
@@ -16,5 +13,5 @@ for i in range(2, int(limit**0.5) + 1):
             is_prime[j] = False
 
 # Sum up the primes below the limit.
-prime_sum = sum(i for i, prime in enumerate(is_prime) if prime)
-print(prime_sum)
+prime_sum = (i for i, prime in enumerate(is_prime) if prime)
+print(sum(prime_sum))
