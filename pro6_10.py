3c8ea330a9bd2121e0a8dad1735ede7b08a6ba9b
Manish Adhikari <adhikari.manish555@gmail.com>
2025-04-04 22:32:26 +0545

    6 and 10
    6 and 10 merged

---
+++ pro6_10.py
@@ -0,0 +1,42 @@
+import math
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+
+'''
+so this is problem 6
+'''
+# count = 0
+# num = 1
+# while count < 10001:
+#     num += 1
+#     if is_prime(num):
+#         count += 1
+
+# print(f"10001st prime is {num}")
+
+'''
+This is problem 10
+'''
+
+count = 0
+num =1
+sum = 0
+
+while count <= 2000000:
+    num += 1
+    if is_prime(num):
+        count += 1
+        sum = sum + num
+
+print(f"the sum is {sum}")
