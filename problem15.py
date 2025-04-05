6b583625ebfa6d4aa5b60c3f82dca8dc6ca9fc99
Manish Adhikari <adhikari.manish555@gmail.com>
2025-04-05 22:34:20 +0545

    Update problem15.py
    multiple problems

--- problem15.py
+++ problem15.py
@@ -1,4 +1,6 @@
-# #p15
+'''
+Problem 15 (inefficient)
+'''
 
 # n = 20
 
@@ -9,8 +11,39 @@
 # # for x in binary_list:
 # #     print(x)
 
+
+'''
+problem 15
+'''
+# import math
+
+# n = 20
+# result = math.comb(2 * n, n)
+# print(result)
+
+
+'''
+problem 16
+'''
+
+# n = 2 ** 1000
+# sum = 0
+# while(n > 0):
+#     sum = sum + n % 10
+#     n //= 10
+
+# print(sum)
+
+'''
+Problem 20
+'''
+
 import math
 
-n = 20
-result = math.comb(2 * n, n)
-print(result)
+n = math.factorial(100)
+sum = 0
+while(n > 0):
+    sum = sum + n % 10
+    n //= 10
+
+print(sum)
